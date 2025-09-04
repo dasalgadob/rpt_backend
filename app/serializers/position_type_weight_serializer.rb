320004a00159d347f70ea1b5b95a96b5cb64a973
Diego Salgado <diego_salgado@apple.com>
2025-09-04 17:55:11 -0500

    update weight serializer

--- app/serializers/position_type_weight_serializer.rb
+++ app/serializers/position_type_weight_serializer.rb
@@ -26,6 +26,26 @@ class PositionTypeWeightSerializer < ActiveModel::Serializer
   attributes :id, :corporate_percentage, :department_percentage, :position_percentage, :job_competencies_percentage, :position_type_id, :position_type_name, :period_id
   has_one :position_type
 
+  def corporate_percentage
+    v = object.corporate_percentage
+    v.nil? ? nil : v.to_f.round(2)
+  end
+
+  def department_percentage
+    v = object.department_percentage
+    v.nil? ? nil : v.to_f.round(2)
+  end
+
+  def position_percentage
+    v = object.position_percentage
+    v.nil? ? nil : v.to_f.round(2)
+  end
+
+  def job_competencies_percentage
+    v = object.job_competencies_percentage
+    v.nil? ? nil : v.to_f.round(2)
+  end
+
   def position_type_name
     object.position_type.name if object.position_type
   end
